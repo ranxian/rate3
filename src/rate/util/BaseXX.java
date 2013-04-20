3f0f96e45780b5ca5fbe11205f0e6a53dd0ae5b3
鲜染 <xranthoar@gmail.com>
2013-04-21 00:21:13 +0800

    .

--- src/rate/util/BaseXX.java
+++ src/rate/util/BaseXX.java
@@ -7,15 +7,14 @@ package rate.util;
  * Time: 下午10:56
  */
 public class BaseXX {
-    private static String map = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM`1234567890-=~!@#$%^&*()_+|[];',./{}:<>?";
-    private static int base = map.length();
+    private static int base = 93;
 
     public static String parse(int num) {
         String res = "";
         while (num!=0) {
             int mod = num % base;
             num /= base;
-            res += map.charAt(mod);
+            res += (char)('!'+mod);
         }
         return res;
     }
