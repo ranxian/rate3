78e435f9edd47b77ffe145c9fefe8ae8d0c14a68
Yu Yuankai on ratedev-server <yykpku@gmail.com>
2013-04-21 01:23:38 +0800

    add examples

---
+++ tools/python/engine/benchmark2bxx.py
@@ -0,0 +1,54 @@
+import sys
+import os
+
+def benchmark2bxx(benchmark_dir):
+    src_file = os.path.join(benchmark_dir, 'benchmark.txt')
+    dst_file = os.path.join(benchmark_dir, 'benchmark_bxx.txt')
+    tmp_file = dst_file+".unsorted"
+    table_file = os.path.join(benchmark_dir, 'uuid_table.txt')
+
+    uuids = {}
+    srcf = open(src_file, 'r')
+    tmpf = open(tmp_file, 'w')
+    tablef = open(table_file, 'w')
+    print "bxxing"
+    while True:
+        l = srcf.readline()
+        if len(l)==0:
+            break
+        l = l.strip()
+        f1 = srcf.readline().strip()
+        f2 = srcf.readline().strip()
+        fs = (f1, f2)
+        (u1, u2, mt) = l.split(' ')
+        us = (u1, u2)
+        for u in us:
+            if u not in uuids:
+                uuids[u] = hex(len(uuids)+1)[2:]
+                print>>tablef, uuids[u], u, fs[us.index(u)]
+        h1 = uuids[u1]
+        h2 = uuids[u2]
+        if h1>h2:
+            h1, h2 = h2, h1
+        print>>tmpf, h1, h2, mt
+
+    srcf.close()
+    tablef.close()
+    tmpf.close()
+
+    print "sorting"
+    os.system('sort %s -o %s' % (tmp_file, dst_file))
+    os.system('rm %s' % tmp_file)
+
+if __name__=='__main__':
+
+    USAGE = """
+    %s benchmark_dir
+    it converts benchmark.txt into benchmark_bxx.txt and uuid_table.txt under benchmark_dir
+    """
+
+    if len(sys.argv)!=2:
+        print USAGE % sys.argv[0]
+
+    benchmark_dir = sys.argv[1]
+    benchmark2bxx(benchmark_dir)
